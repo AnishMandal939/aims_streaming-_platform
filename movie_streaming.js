8f1b617ffff77057e62e1b3e486580ebc7ed0b18
Anish Mandal <mandalktaanees12@gmail.com>
2023-07-24 13:43:38 +0545

    streaminng platform completed ready to host

--- movie_streaming.js
+++ movie_streaming.js
@@ -2,133 +2,15 @@
 const apikey="0818b83e0b90ab1e7b053d86806fccda";
 const apiEndpoint = "https://api.themoviedb.org/3";
 const imgPath = "https://image.tmdb.org/t/p/original";
+const youtube_api = "AIzaSyC3VZl56JpTlevvLKvJKzVsljLpvb_i2jg";
 
-
-const apiPaths = {
-    fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}&language=en-US`,
-    fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
-}
-
-// boots up app
-function init(){
-    fetchAndBuildAllSections(); // calling
-}
-
-function fetchAndBuildAllSections(){
-    fetch(apiPaths.fetchAllCategories)
-    .then(res => res.json())
-    .then(res => {
-        const categories = res.genres;
-        // check if array
-        if(Array.isArray(categories) && categories.length){
-            categories.forEach((category, i) => {
-            // categories.slice(0,3).forEach((category, i) => {
-                // fetchAndbuildMovieSection(category);
-                fetchAndbuildMovieSection(
-                    apiPaths.fetchMoviesList(category.id),
-                     category);
-
-            });
-        } // check if array and not empty
-        // console.table(categories);
-    } )
-    .catch(err => console.error('error from api',err))
-}
-// fetch and buildMovieSection
-function fetchAndbuildMovieSection(fetchUrl,category){
-    // console.log('category', category, fetchUrl);
-    fetch(fetchUrl)
-    .then(res => res.json())
-    .then(res => {
-        console.table(res.results);
-        const movies = res.results;
-        if(Array.isArray(movies) && movies.length){
-            buildMoviesSection(movies.slice(0,6),category.name);
-        }
-        // const {results} = res;
-    })
-    .catch(err => console.error('error from api',err))
-}
-
-// build movies section
-function buildMoviesSection(list, categoryName){
-    console.log(list, categoryName);
-
-    const moviesCont =  document.getElementById('movies-cont');
-
-    const moviesListHTML = list.map(item => {
-        return `
-        <div class="movie-item" >
-            <img class="move-item-img" src="${imgPath}${item.backdrop_path}" alt="${item.title}" />
-            <div class="iframe-wrap" id="yt${item.id}"></div>
-        </div>`;
-    }).join('');
-
-    const moviesSectionHTML = `
-        <h2 class="movie-section-heading">${categoryName} <span class="explore-nudge">Explore All</span></span></h2>
-        <div class="movies-row">
-            ${moviesListHTML}
-        </div>
-    `
-
-    const div = document.createElement('div');
-    div.className = "movies-section"
-    div.innerHTML = moviesSectionHTML;
-
-    // append html into movies container
-    moviesCont.append(div);
-
-
-}
-window.addEventListener("load", function(){
-    init();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// finished code 
-
-/**
- * 
- * // Consts
-const apikey = "7543524441a260664a97044b8e2dc621";
-const apiEndpoint = "https://api.themoviedb.org/3"
-const imgPath = "https://image.tmdb.org/t/p/original";
-
+// AIzaSyDIrCeeZqsEpqgGUH53OX8uc98WznDy4iI - youtube
 
 const apiPaths = {
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
     fetchTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
-    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`
+    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${youtube_api}`
 }
 
 
@@ -154,10 +36,11 @@ function buildBannerSection(movie){
     bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
 
     const div = document.createElement('div');
+    //check for null or undefined with nullish coalescing operator - ?? is used to check for null or undefined
 
     div.innerHTML = `
-            <h2 class="banner__title">${movie.title}</h2>
-            <p class="banner__info">Trending in movies | Released - ${movie.release_date} </p>
+            <h2 class="banner__title">${movie.title ?? ""}</h2>
+            <p class="banner__info">Trending in movies | Released - ${movie.release_date ?? ""} </p>
             <p class="banner__overview">${movie.overview && movie.overview.length > 200 ? movie.overview.slice(0,200).trim()+ '...':movie.overview}</p>
             <div class="action-buttons-cont">
                 <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
@@ -189,7 +72,7 @@ function fetchAndBuildAllSections(){
 }
 
 function fetchAndbuildMovieSection(fetchUrl, categoryName){
-    console.log(fetchUrl,categoryName);
+    // console.log(fetchUrl,categoryName);
     return fetch(fetchUrl)
     .then(res => res.json())
     .then(res => {
@@ -213,6 +96,7 @@ function buildMoviesSection(list, categoryName){
         <div class="movie-item" onmouseenter="searchMovieTrailer('${item.title}', 'yt${item.id}')">
             <img class="move-item-img" src="${imgPath}${item.backdrop_path}" alt="${item.title}" />
             <div class="iframe-wrap" id="yt${item.id}"></div>
+            <span class="movie-name">${item.title}</span>
         </div>`;
     }).join('');
 
@@ -243,7 +127,7 @@ function searchMovieTrailer(movieName, iframId) {
         console.log(elements, iframId);
 
         const div = document.createElement('div');
-        div.innerHTML = `<iframe width="245px" height="150px" src="https://www.youtube.com/embed/${bestResult.id.videoId}?autoplay=1&controls=0"></iframe>`
+        div.innerHTML = `<iframe width="245px" height="150px" allowfullscreen= “true” src="https://www.youtube.com/embed/${bestResult.id.videoId}?&autoplay=1&mute=1&controls=1"></iframe>`
 
         elements.append(div);
         
@@ -251,9 +135,31 @@ function searchMovieTrailer(movieName, iframId) {
     .catch(err=>console.log(err));
 }
 
+// Search
+function liveSearch() {
+    // Locate the card elements
+    let cards = document.querySelectorAll('.movie-item')
+    // Locate the search input
+    let search_query = document.getElementById("searchbox").value;
+    // Loop through the cards
+    for (var i = 0; i < cards.length; i++) {
+      // If the text is within the card...
+      if(cards[i].innerText.toLowerCase()
+        // ...and the text matches the search query...
+        .includes(search_query.toLowerCase())) {
+          // ...remove the `.is-hidden` class.
+          cards[i].classList.remove("is-hidden");
+      } else {
+        // Otherwise, add the class.
+        cards[i].classList.add("is-hidden");
+      }
+    }
+  }
+
 
 window.addEventListener('load',function() {
     init();
+    liveSearch();
     window.addEventListener('scroll', function(){
         // header ui update
         const header = document.getElementById('header');
@@ -261,5 +167,3 @@ window.addEventListener('load',function() {
         else header.classList.remove('black-bg');
     })
 })
-
- */
